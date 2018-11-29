fba77ed6ef140fb5b2c665ec9d6891c457949221
DanielCeballosJ <dceballosj@gmail.com>
2018-11-29 10:58:44 +0100

    Update dataLoader.py
    update dataLoader

--- src/dataLoader.py
+++ src/dataLoader.py
@@ -1,61 +1,72 @@
-import numpy as np
+from skimage import io, transform
+import os
 import keras
-import linecache
-import cv2
-
+import pandas as pd
+import numpy as np
 class DataGenerator(keras.utils.Sequence):
-    'Generates data for Keras'
-    def __init__(self, list_IDs, batch_size=32, dim=(32,32,32), n_channels=1,
-                 path='../Dataset/trainingtraining_set_pixel_size_and_HC.csv', shuffle=True):
-        'Initialization'
-        self.dim = dim
+    def __init__(self, csv_file, root_dir, batch_size, transform=None, output_size=512):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.hc_grame = pd.read_csv(csv_file)
+        self.root_dir = root_dir
         self.batch_size = batch_size
-        self.list_IDs = list_IDs
-        self.n_channels = n_channels
-        self.path = path
-        self.shuffle = shuffle
-        self.on_epoch_end()
+        self.tranform = transform
+
 
     def __len__(self):
-        'Denotes the number of batches per epoch'
-        return int(np.floor(len(self.list_IDs) / self.batch_size))
+        return len(self.hc_frame)
 
-    def __getitem__(self, index):
-        'Generate one batch of data'
-        # Generate indexes of the batch
-        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+    def __getitem__(self, idx):
+        batch_x = self.hc_frame.iloc[idx * self.batch_size:(idx + 1) * self.batch_size, 0]
+        batch_y = self.hc_frame.iloc[idx * self.batch_size:(idx + 1) * self.batch_size, 0].str.replace('.png', '_HC_Annotation.png')
 
-        # Find list of IDs
-        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+        # head circumference in pixel = head circumference (mm) // pixel_size(mm)
+        batch_hc = self.hc_frame[idx * self.batch_size:(idx + 1) * self.batch_size, 2] // \
+                   self.hc_frame[idx * self.batch_size:(idx + 1) * self.batch_size, 2]
 
-        # Generate data
-        X, Y = self.__data_generation(list_IDs_temp)
 
-        return X, Y
+        img_name = os.path.join(self.root_dir,
+                                self.hc_frame.iloc[idx, 0])
 
-    def on_epoch_end(self):
-        'Updates indexes after each epoch'
-        self.indexes = np.arange(len(self.list_IDs))
-        if self.shuffle == True:
-            np.random.shuffle(self.indexes)
+        # read input image x
+        X = np.array([self.image_transformer(io.imread(file_name))
+                      for file_name in batch_x])
+        # read output image y
+        Y = np.array([self.image_transformer(io.imread(file_name))
+                     for file_name in batch_y])
 
-    def __data_generation(self, list_IDs_temp):
-        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
-        # Initialization
-        X = list()
-        Y = list()
+        # batch_hc to numpy array
+        HC = np.array(batch_hc)
 
-        # Generate data
-        for i, ID in enumerate(list_IDs_temp):
-            # read line from csv
-            data = linecache.getline('foo.csv', i).split(',')
+        return X, Y, HC
 
-            # append output (input image)
-            img_x = cv2.imread(self.path + 'set/' + data[i,0])
-            X.append({'img_x': img_x})
+    # transformer for image augmentation
+    def image_transformer(self, image):
+        # reshape image
+        if 'reshape' in self.transform.keys():
+            image = self.reshape(image, self.tranform['reshape'])
+        return image
 
-            # append output (segmantated image), (head circumference (mm)), (pixel size(mm))
-            img_y =  cv2.imread(self.path + 'set/' + data[i,0].replace('.png', '') + '_HC_Annotation.png')
-            Y.append({'img_y': img_y, 'hc': data[i,2], 'pixel': data[i,1]})
 
-        return X, Y
+    def reshape(self, img, output_size):
+        assert isinstance(output_size, (int, tuple))
+        h, w = img.shape[:2]
+        if isinstance(output_size, int):
+            if h > w:
+                new_h, new_w = output_size * h / w, output_size
+            else:
+                new_h, new_w = output_size, output_size * w / h
+        else:
+            new_h, new_w = output_size,output_size
+
+        new_h, new_w = output_size, output_size
+        new_h, new_w = int(new_h), int(new_w)
+
+        return transform.resize(img, (new_h, new_w))
+
+
